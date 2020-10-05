2385bf11ffb9b55cdc26ab3cda74019e5fa2c76e
JJ Sermon <thejoshuasermon@gmail.com>
2020-10-05 16:12:06 -0400

    if statement

--- app.py
+++ app.py
@@ -23,10 +23,55 @@ def say_hi(name, age):
 say_hi("Josh", "26")
 say_hi("Jacob", "44")
 
-'''
+------------------------------------------------------
+Return Statement
 
 def cube(num):
    return num*num*num
 
 result = (cube(4))
 print(result)
+
+-------------------------------------------------------
+If Statements
+
+is_male = True
+is_tall = True
+
+if is_male and is_tall:
+   print("You are a tall male")
+elif is_male and not(is_tall):
+   print("You are a short male")
+elif not(is_male) and is_tall:
+   print("You are not a male but are tall")
+else:
+   print("You are not a male and not tall")
+'''
+
+------------------------------------------------------
+Comparisons
+
+def max_num(num1, num2, num3):
+   if  num1 >= num2 and num1 >= num3:
+      return num1
+   elif num2 >= num1 and num2 >= num3:
+      return num2
+   else:
+      return num3
+
+print(max_num(300, 40, 5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
