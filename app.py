72c8b3bee55ced28f97ba301cf2449e829d7d0e7
JJ Sermon <thejoshuasermon@gmail.com>
2020-10-05 06:51:14 -0400

    return statement

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
-friends[1] = "Mike"
-print(friends[1])
+'''
+------------------------------------------------------
+List Functions
+
+lucky_numbers = [42, 8, 15, 16, 23]
+friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Toby"]
+
+friends2 = friends.copy()
+
+print(friends2)
+------------------------------------------------------
+Tuples
+
+coordinates = (4, 5)
+
+print(coordinates[1])
+-------------------------------------------------------
+Functions
+
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age))
+
+say_hi("Josh", "26")
+say_hi("Jacob", "44")
+
+'''
+
+def cube(num):
+   return num*num*num
+
+result = (cube(4))
+print(result)
