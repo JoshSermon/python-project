804375b5042eb42ee183ba91af5b684ff110d4c1
JJ Sermon <thejoshuasermon@gmail.com>
2020-10-08 08:51:20 -0400

    Guessing Game - Variables, While Loops.

--- app.py
+++ app.py
@@ -98,7 +98,8 @@ monthConversions = {
 
 print(monthConversions.get("Dec"))
 
-'''
+---------------------------------------------
+While Loop
 
 i = 1
 while i <= 10:
@@ -107,7 +108,23 @@ while i <= 10:
 
 print("Done with loop")
 
+'''
 
+secret_word = "ball"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
+while guess != secret_word and not(out_of_guesses):
+   if guess_count < guess_limit:
+      guess = input("Enter guess: ")
+      guess_count += 1
+   else:
+      out_of_guesses = True
 
+if out_of_guesses:
+   print("Out of Guesses, YOU LOSE!")
+else:
+   print("You win!")
 
